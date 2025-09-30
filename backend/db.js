b2a351c75874fe2d44671af5bad1e61f971ee658
MayurNikam989 <98411442+MayurNikam989@users.noreply.github.com>
2025-09-30 14:24:54 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/?readPreference=primary";
+const mongoURI = process.env.MONGO_URI;
 const connectToMongo = () => {
   //connecting database
   mongoose.connect(mongoURI);
