2e2ffe183361effdf61c9309a81add36287a9111
MayurNikam989 <98411442+MayurNikam989@users.noreply.github.com>
2025-09-30 14:47:48 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,6 +3,7 @@ const mongoURI = process.env.MONGO_URI;
 const connectToMongo = () => {
   //connecting database
   mongoose.connect(mongoURI);
+  mongoose.set('autoIndex', false);
 };
 
 module.exports = connectToMongo;
